a49161c6a5187b7efef538d0e99ee04416c48e3e
Shane Andrade <shane.andrade@gmail.com>
2013-11-12 18:27:27 -0800

    heroku stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,10 @@
 import os
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
-from flask_environments import Environments
 
 app = Flask(__name__)
-env = Environments(app)
 
-env.from_yaml(os.path.join(os.getcwd(), 'config.yaml'))
-
+app.config.from_object('config')
 db = MongoEngine(app)
 
 from app import views
