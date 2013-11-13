01775ccc803f247e7c4e1a95f7e434ded207c0b7
Shane Andrade <shane.andrade@gmail.com>
2013-11-12 18:48:19 -0800

    fix

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-env = os.environ['FLASK_ENV']
+env = os.environ.get('FLASK_ENV', 'DEVELOPMENT')
 
 CSRF_ENABLED=True
 SECRET_KEY='622fa765fa110611a164164fa347e941687cb7711c4ad6f1017a448cdd4c8c64'
@@ -7,9 +7,19 @@ DEBUG=True
 TESTING=True
 
 if env == 'PRODUCTION':
+  from pymongo.uri_parser import parse_uri
+
+  uri = os.environ['MONGOHQ_URL']
+  parsed = parse_uri(uri)
+
+  host, port = parsed.get('nodelist')[0]
+
   MONGODB_SETTINGS = {
-    'DB': os.environ['MONGO_DB'],
-    'host': os.environ['MONGOHQ_URL']
+    'DB': parsed.get('database'),
+    'HOST': host,
+    'PORT': port,
+    'USERNAME': parsed.get('username'),
+    'PASSWORD': parsed.get('password')
   }
 else:
   MONGODB_SETTINGS = {
